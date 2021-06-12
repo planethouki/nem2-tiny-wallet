33332eba18d9deb2ac284d83444205d11f9070e5
planet★箒星 <26595148+planethouki@users.noreply.github.com>
2021-06-12 13:07:52 +0900

    test split

---
+++ test/jest/independence.test.js
@@ -0,0 +1,26 @@
+const independence = require('../../src/utils/independence')
+
+describe('independence', () => {
+    test('endian', () => {
+        const { endian } = independence;
+        expect(endian("5B66E76BECAD0860")).toBe("6008ADEC6BE7665B");
+        expect(endian("EEAFF441BA994BE7")).toBe("E74B99BA41F4AFEE");
+    });
+
+    test('uint8ArrayToHex', () => {
+        const { uint8ArrayToHex } = independence;
+        expect(uint8ArrayToHex(new Uint8Array([0, 1, 2]))).toBe("000102");
+        expect(uint8ArrayToHex(new Uint8Array([255, 254]))).toBe("FFFE");
+    });
+
+    test('hexToUint8Array', () => {
+        const { hexToUint8Array } = independence;
+        const a = hexToUint8Array("000102");
+        expect(a).toBeInstanceOf(Uint8Array);
+        expect(a.length).toBe(3);
+        expect(a).toContain(0);
+        expect(a).toContain(1);
+        expect(a).toContain(2);
+    });
+
+});
